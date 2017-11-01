078cdcbfa8e6076ef8423c512d7fb77a51d454b1
SiZY <scar.syler@gmail.com>
2017-11-01 14:12:42 +0800

    Ex5 works!

--- LED_Control/main.c
+++ LED_Control/main.c
@@ -9,6 +9,7 @@
 #include <math.h>
 #include <tms320.h>
 #include <dsplib.h>
+#include <Dsplib.h>
 #include "firc.h"
 
 #define TCR0		*((ioport volatile Uint16 *) 0x1810)
@@ -16,37 +17,32 @@
 #define TIME_START	0x8001
 #define TIME_STOP	0x8000
 
-DATA LP[TAPS] = { 
+Int16 LP[TAPS] = { 
 	#include "lpf.dat" 
 };
 
-Uint16 in_left[TAPS]/* = {0}*/;
-Uint16 in_right[TAPS]/* = {0}*/;
-
-DATA dbuffer_left[TAPS + 2]/* = {0}*/;
-DATA dbuffer_right[TAPS + 2]/* = {0}*/;
-
 void main(void)
 {
-	DATA right, left; //AIC inputs
-	DATA out_left, out_right; //AIC output
-	Uint16 start_time;
-	Uint16 end_time;
-	Uint16 delta_time;
-	USBSTK5515_init(); 	//Initializing the Processor
-	AIC_init(); 		//Initializing the Audio Codec
+	Int16 right[1], left[1]; 					//AIC inputs
+	Int16 out_left[1], out_right[1]; 			//AIC output
+	Int16 dbuffer_left[TAPS + 2], dbuffer_right[TAPS + 2]; 	//debuffer
+//	Uint16 start_time;
+//	Uint16 end_time;
+//	Uint16 delta_time;
+	USBSTK5515_init(); 					//Initializing the Processor
+	AIC_init(); 						//Initializing the Audio Codec
 	
 
 
 	
 	while(1)
 	{
-		AIC_read2(&right, &left);
-		start_time = TIMCNT1_0;
-		fir(&right, &LP, &out_right, &dbuffer_right, 1, TAPS);
-		fir(&left, &LP, &out_left, &dbuffer_left, 1, TAPS);
-		end_time = TIMCNT1_0;
-		delta_time = start_time - end_time;
-		AIC_write2(out_right, out_left);
+		AIC_read2(&right[0], &left[0]);
+//		start_time = TIMCNT1_0;
+		fir(&right[0], &LP[0], &out_right[0], &dbuffer_right[0], 1, TAPS);
+		fir(&left[0], &LP[0], &out_left[0], &dbuffer_left[0], 1, TAPS);
+//		end_time = TIMCNT1_0;
+//		delta_time = start_time - end_time;
+		AIC_write2(out_right[0], out_left[0]);
 	}
 }
